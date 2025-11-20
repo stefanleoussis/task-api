2d62cacb3cb29f7f6d10394eaa5ee61412673182
Stefan <58789967+stefanleoussis@users.noreply.github.com>
2025-11-20 10:45:31 -0800

    Add deployed URL

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://task-frontend-six-sepia.vercel.app",
+    ],
   })
 );
 
